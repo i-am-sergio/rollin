24299c539d2bb8a8cf6af2e1cd485874331b86bc
ShinjiMC <nayapbrau@gmail.com>
2024-02-19 23:43:13 -0500

    Add ReaderPDF with method POST in JS

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 An online registration system for events, managing participant sign-ups on a first-come, first-served basis. Ideal for conferences, workshops, and courses with limited capacity, ensuring fair and efficient registration processes
 
-npm install express pdfjs-dist fs
+npm install express pdfjs-dist multer
 
 npm run dev
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const fs = require("fs").promises;
-
+const multer = require("multer");
 const app = express();
 const port = 5000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const date = new Date();
+    const timestamp = date.getTime();
+    const originalname = file.originalname;
+    const filename = `${timestamp}-${originalname}`;
+    cb(null, filename);
+  },
+});
+const upload = multer({ storage: storage });
 
 async function getItems(src) {
   try {
@@ -10,34 +23,50 @@ async function getItems(src) {
     const pdf = await pdfjs.getDocument(src).promise;
     const page = await pdf.getPage(1);
     const content = await page.getTextContent();
-    const items = content.items.map((item) => item.str);
-    return items;
+    return content.items.map((item) => item.str);
   } catch (error) {
     console.error("Error al obtener elementos del PDF:", error);
     throw error;
   }
 }
 
-async function saveItemsToFile(items, filePath) {
+async function extractCourses(items) {
   try {
-    const text = items.join("\n");
-    await fs.writeFile(filePath, text);
-    console.log("Items guardados en constancia.txt");
+    const extractedCourses = [];
+    let currentIndex = 71;
+    while (currentIndex < items.length) {
+      const currentCourse = items[currentIndex];
+      if (
+        currentCourse === currentCourse.toUpperCase() &&
+        currentCourse !== " "
+      )
+        extractedCourses.push(currentCourse);
+      currentIndex += 17;
+    }
+    return extractedCourses;
   } catch (error) {
-    console.error("Error al guardar los items:", error);
+    console.error("Error al extraer los cursos:", error);
+    throw error;
   }
 }
 
 app.get("/", async (req, res) => {
   try {
-    // Ruta del archivo PDF
-    const pdfPath = "constancia.pdf";
-
-    // Obtener los elementos del PDF
+    const pdfPath = "20210689.pdf";
     const items = await getItems(pdfPath);
+    const extractedCourses = await extractCourses(items);
+    res.send(extractedCourses);
+  } catch (error) {
+    res.status(500).send("Error interno del servidor");
+  }
+});
 
-    // Enviar los elementos como respuesta
-    res.send(items);
+app.post("/upload", upload.single("constancia"), async (req, res) => {
+  try {
+    const pdfPath = req.file.path;
+    const items = await getItems(pdfPath);
+    const extractedCourses = await extractCourses(items);
+    res.send(extractedCourses);
   } catch (error) {
     res.status(500).send("Error interno del servidor");
   }

dd9547ce332e1ea2d75e23d54adb73178d0c973c
sergiodev <sergio15.dev@gmail.com>
2024-02-21 21:21:34 -0500

    add filecontroller and fileservice

--- server/app/routes/FileRoute.js
+++ server/app/routes/FileRoute.js
@@ -1,13 +1,31 @@
 import express from 'express'
+import { getItems, uploadFile } from '../controllers/FileController.js';
+import multer from 'multer'
 
 const router = express.Router()
 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public/");
+    },
+    filename: function (req, file, cb) {
+      const date = new Date();
+      const timestamp = date.getTime();
+      const originalname = file.originalname;
+      const filename = `${timestamp}-${originalname}`;
+      cb(null, filename);
+    },
+  });
+
+const upload = multer({ storage: storage });
+
 router.get('/', (req, res) => {
-    res.send('Hola mundo')
+    res.send('Hello World!')
 })
 
-router.post('/upload', (req, res) => {
-    res.send('Archivo subido');
-})
+router.get('/getitems', getItems)
+
+router.post('/upload', upload.single("constancia"), uploadFile )
 
 export default router

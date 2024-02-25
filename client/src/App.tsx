34b667d941259a14c065a95dc1aa086eb73e4566
sergiodev <sergio15.dev@gmail.com>
2024-02-24 21:17:50 -0500

    add client files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,49 @@
+import React from "react";
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth/Auth";
+import { useSelector } from "react-redux";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+function App(): JSX.Element {
+  const user = useSelector((state: any) => state.authReducer.authData);
+
+  return (
+    <div
+      className="App"
+      style={{
+        height:
+          window.location.href === "http://localhost:3000/chat"
+            ? "calc(100vh - 2rem)"
+            : "auto",
+      }}
+    >
+      <div className="blur" style={{ top: "70%", right: "-2rem" }}></div>
+      <div className="blur" style={{ top: "20%", left: "-6rem" }}></div>
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
